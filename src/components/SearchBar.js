cd3f4607d368a932f18a93b1d816761d73e83817
Laura Badgley <l.badgleyy@gmail.com>
2020-01-15 16:24:07 -0800

    event listeners on form and  searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { searchTerm: '' };
+
+  onInputChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    
+    //TODO make sure we call callback from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>
+    return ( 
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input 
+              type="text"
+              value={this.state.searchTerm} 
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
