fca75ff1a6bc0f3385e980f07060e2029055f706
Laura Badgley <l.badgleyy@gmail.com>
2020-01-19 23:44:55 -0800

    built the video detail component and worked on styling the video items

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <span>Loading... please wait</span>
+  }
+
+  const videoSrc= `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div className="ui segment">
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+      <h4 className="ui header"> {video.snippet.title} </h4>
+      <p>{video.snippet.description}</p>
+    </div>
+  ) 
+};
+
+export default VideoDetail;
