b0c770d11bfc3437f09ab3ac3327da497a304f7d
Laura Badgley <l.badgleyy@gmail.com>
2020-01-19 23:44:03 -0800

    added onVideoSelect in order to interact with the video links

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,12 @@
 import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
-      <div className="content">
-        <div className="header">{video.snippet.title}</div>
+    <div onClick={on => {onVideoSelect(video)}} className="video-item item" >
+      <img className="ui image" id="imageDiv" src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
+      <div className="content" >
+        <div className="header" id="headerDiv">{video.snippet.title}</div>
       </div>
     </div>
   )
