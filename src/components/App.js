7beb895d2b43d95dfe7bdc3bce0b255ee2026d10
Laura Badgley <l.badgleyy@gmail.com>
2020-02-04 11:14:23 -0800

    added semantic UI grid styling for video player

--- src/components/App.js
+++ src/components/App.js
@@ -26,9 +26,17 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onSearchSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} alt={''} />
+          <SearchBar onFormSubmit={this.onSearchSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} alt={''} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
